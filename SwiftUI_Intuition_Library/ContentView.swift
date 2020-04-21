471a782710aedeacb8bc498499eb29918f2f5916
Jacob_d <zjc8966@me.com>
2020-04-21 17:19:36 +0800

    Initial Commit

---
+++ SwiftUI_Intuition_Library/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Intuition_Library
+//
+//  Created by Jacob Zhang on 2020/4/21.
+//  Copyright © 2020 Jacob Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
